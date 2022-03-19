398db67162540a85b380bbf8c9a4de4889b6ba13
proxy <1106842466@qq.com>
2022-03-19 15:38:38 +0800

    init

---
+++ src/main/java/com/proxyy/jacksonextplugin/JacksonExtPluginApplication.java
@@ -0,0 +1,13 @@
+package com.proxyy.jacksonextplugin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JacksonExtPluginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JacksonExtPluginApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/proxyy/jacksonextplugin/JacksonExtPluginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proxyy.jacksonextplugin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JacksonExtPluginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
